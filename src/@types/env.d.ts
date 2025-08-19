096662844a02847f6b3dfaca4c62b38b3e821326
Felipe Alexandre <felipealexandre@INV001575.local>
2025-08-19 09:02:21 -0300

    feat: using dotenv to read env and changing file to upload to google to aab

---
+++ src/@types/env.d.ts
@@ -0,0 +1,3 @@
+declare module "@env" {
+  export const WEATHER_APP_ID: string;
+}
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { WEATHER_APP_ID } from "@env";
 
 export const api = axios.create({
   baseURL: "https://api.openweathermap.org/data/2.5",
   params: {
     lang: 'pt_br',
     units: 'metric',
-    appid: process.env.WEATHER_APP_ID,
+    appid: WEATHER_APP_ID,
   }
 });
